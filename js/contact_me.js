1773a7afa623cc1c10540c63b57ce197bfc4eb54
jshsakura <jshsakura@c-node.com>
2018-07-01 18:09:57 +0900

    문의내역 전송문구 수정

--- js/contact_me.js
+++ js/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>문의 내역 전송 완료되었습니다. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
